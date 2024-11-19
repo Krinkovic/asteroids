0550f7425af44c0f9213fd10d97fdbc7180bdebd
Krinkovic <kristoffer90forsberg@gmail.com>
2024-11-19 19:45:39 +0100

    Added a triangle player that can rotate

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	clock = pygame.time.Clock()
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+	dt = 0
+
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
@@ -12,8 +17,15 @@ def main():
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
+
 		screen.fill("black")
+		player.draw(screen)
 		pygame.display.flip()
+
+		dt = clock.tick(60) / 1000
+		
+		player.update(dt)
+	
 
 if __name__ == "__main__":
 	main()
